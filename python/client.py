74ced421e3b9cb6be73fb6d78298bd5d89e0bf1b
Frederico Dôndici Gama Vieira <fredericodgv@gmail.com>
2023-09-01 15:57:00 -0300

    adiciona separação de comandos

--- python/client.py
+++ python/client.py
@@ -7,10 +7,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     client.connect((HOST, PORT))
     # client.sendall(b'AAAAAAAAAAAAAAAAAAAAAAA')
     while True:
-        message = input("Insira uma mensagem (ou escreva quit para sair): ")
-       
-        if message == "quit":
+        data = input("Escreva echo para enviar uma mensagem ou quit para sair:")
+        data = data.strip().lower()
+        if not data:
+            break   
+
+        if data == "quit":
             break
+
+        if data == "echo":
+            message = input("Insira uma mensagem (ou escreva quit para sair): ")
+       
         client.sendall(message.encode())
         data = client.recv(1024).decode()
         print(f"Received from server: {data}")
