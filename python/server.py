0453da533ca2b27729a6b8d7b7abc6add3321ddb
nicolassoam <nsm4456@gmail.com>
2023-09-01 14:23:08 -0300

    adicionando multithread no python

--- python/server.py
+++ python/server.py
@@ -1,8 +1,19 @@
 import socket
 import sys
+import threading
 
 HOST = '127.0.0.1'  # Localhost
 PORT = 8080
+
+def client_handle(connection, address):
+    # Receive and send data
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(data)
+        # Close the connection
+        connection.close()
 
 try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -11,18 +22,12 @@ try:
         print(f"Server listening on {HOST}:{PORT}")
         
         # Accept incoming connections
-        connection, address = server_socket.accept()
-        print(f"Connected by {address}")
-        
-        # Receive and send data
         while True:
-            data = connection.recv(1024)
-            if not data:
-                break
-            connection.sendall(data)
-            
-        # Close the connection
-        connection.close()
+            connection, address = server_socket.accept()
+            print(f"Connected by {address}")
+            client_handler = threading.Thread(target=client_handle, args=(connection, address))
+            client_handler.start()
+        
 except socket.error as e:
     print(f"Error occurred: {e}")
     sys.exit(1)
